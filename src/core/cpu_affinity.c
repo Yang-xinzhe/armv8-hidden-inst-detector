bbf9f0f0085e6164a824789bdeb5e85837d8cc33
Yangxinzhe <1245314699@qq.com>
2025-12-02 14:30:28 +0800

    feat: set cpu affinity api

---
+++ src/core/cpu_affinity.c
@@ -0,0 +1,13 @@
+#include "cpu_affinity.h"
+
+int set_cpu_affinity(pid_t pid, int core_id) {
+    cpu_set_t mask;
+    CPU_ZERO(&mask);
+    CPU_SET(core_id, &mask);
+
+    if (sched_setaffinity(pid, sizeof(mask), &mask) < 0) {
+        perror("Setting CPU affinity failed");
+        return -1;
+    }
+    return 0;
+}
