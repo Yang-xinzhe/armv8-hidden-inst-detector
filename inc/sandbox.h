36401b3d9b4a314a33e19d9c9b806d8c33a6b421
Yangxinzhe <1245314699@qq.com>
2025-12-04 20:02:52 +0800

    feat: init_insn_page execute_insn_page api available

--- inc/sandbox.h
+++ inc/sandbox.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "core.h"
+#include "register_states.h"
 
 #define PAGE_SIZE   4096
 #define MY_SIGSTKSZ 8192
@@ -13,6 +14,8 @@ extern volatile sig_atomic_t timeout_occurred;
 extern uint32_t insn_offset;
 extern uint32_t mask;
 
+extern char boilerplate_start, boilerplate_end, insn_location;
+
 extern sigjmp_buf escape_env;
 extern timer_t watchdog_timer;
 
@@ -21,6 +24,10 @@ extern stack_t sig_stack;
 
 void signal_handler(int, siginfo_t *, void*);
 void init_signal_handler(void(*handler)(int, siginfo_t*, void*), int, int);
+
+int init_insn_page(void);
+void execute_insn_page(uint8_t*, size_t, RegisterStates *states);
+size_t fill_insn_buffer(uint8_t*, size_t, uint32_t);
 
 int init_watchdog_timer(void);
 void arm_watchdog_us(int us);
