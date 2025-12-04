48c803ce62a9253afddcf25ea2831f890ff63e57
Yangxinzhe <1245314699@qq.com>
2025-12-04 20:04:39 +0800

    refactor: move init/execute_insn_page api to sandbox

--- src/phase1_screening/worker_screen.c
+++ src/phase1_screening/worker_screen.c
@@ -3,11 +3,6 @@
 #include "bitmap.h"
 
 void execution_boilerplate(void);
-int init_insn_page(void);
-void execute_insn_page(uint8_t*, size_t);
-size_t fill_insn_buffer(uint8_t*, size_t, uint32_t);
-
-extern char boilerplate_start, boilerplate_end, insn_location;
 
 void execution_boilerplate(void)
 {
@@ -67,105 +62,6 @@ void execution_boilerplate(void)
 
 }
 
-int init_insn_page(void)
-{
-    // Allocate an executable page / memory region
-    insn_region = mmap(NULL,
-                       PAGE_SIZE * 3,
-                       PROT_NONE,
-                       MAP_PRIVATE | MAP_ANONYMOUS,
-                       -1,
-                       0);
-
-    if (insn_region == MAP_FAILED)
-        return 1;
-
-    insn_page = (uint8_t*)insn_region + PAGE_SIZE;
-
-    if (mprotect(insn_page, PAGE_SIZE, PROT_READ | PROT_WRITE | PROT_EXEC) != 0) {
-        munmap(insn_region, PAGE_SIZE * 3);
-        return 1;
-    }
-
-    uint32_t boilerplate_length = (&boilerplate_end - &boilerplate_start) / 4;
-
-    // Load the boilerplate assembly
-    uint32_t i;
-    for (i = 0; i < boilerplate_length; ++i)
-        ((uint32_t *)insn_page)[i] = ((uint32_t *)&boilerplate_start)[i];
-
-    insn_offset = (&insn_location - &boilerplate_start) / 4;
-
-    if (mprotect(insn_page, PAGE_SIZE, PROT_READ | PROT_EXEC) != 0) {
-        munmap(insn_region, PAGE_SIZE * 3);
-        return 1;
-    }
-
-    return 0;
-}
-
-void execute_insn_page(uint8_t *insn_bytes, size_t insn_length)
-{
-    // Jumps to the instruction buffer
-    void (*exec_page)() = (void (*)())insn_page;
-
-    if (mprotect(insn_page, PAGE_SIZE, PROT_READ | PROT_WRITE | PROT_EXEC) != 0) {
-        perror("mprotect RWX failed");
-        return;
-    }
-
-    // Update the first instruction in the instruction buffer
-    memcpy(insn_page + insn_offset * 4, insn_bytes, insn_length);
-
-    last_insn_signum = 0;
-    timeout_occurred = 0;
-
-    /*
-     * Clear insn_page (at the insn to be tested + the msr insn before)
-     * in the d- and icache
-     * (some instructions might be skipped otherwise.)
-     */
-    __builtin___clear_cache(insn_page + (insn_offset - 1) * 4,
-                  insn_page + insn_offset * 4 + insn_length);
-
-    executing_insn = 1;
-
-    // Jump to the instruction to be tested (and execute it)
-    if(sigsetjmp(escape_env, 1) == 0) {
-        arm_watchdog_us(200);
-
-        exec_page(); 
-
-        disarm_watchdog();
-    } else {
-        disarm_watchdog();
-
-        if (timeout_occurred) {
-            last_insn_signum = SIGALRM;
-        }
-    }
-    
-    executing_insn = 0;
-
-    if (mprotect(insn_page, PAGE_SIZE, PROT_READ | PROT_WRITE | PROT_EXEC) != 0) {
-        perror("mprotect restore RWX failed");
-    }
-
-}
-
-size_t fill_insn_buffer(uint8_t *buf, size_t buf_size, uint32_t insn)
-{
-    if (buf_size < 4)
-        return 0;
- 
-    else {
-        buf[0] = insn & 0xff;
-        buf[1] = (insn >> 8) & 0xff;
-        buf[2] = (insn >> 16) & 0xff;
-        buf[3] = (insn >> 24) & 0xff;
-    }
-    return 4;
-}
 
 static int count_ranges_in_file(FILE *f, uint64_t *total_insns_out)
 {
@@ -319,7 +215,7 @@ int main(int argc, const char* argv[]) {
             uint8_t insn_bytes[4];
             size_t buf_len = fill_insn_buffer(insn_bytes, sizeof(insn_bytes), insn);
 
-            execute_insn_page(insn_bytes, buf_len);
+            execute_insn_page(insn_bytes, buf_len, NULL);
 
             if (last_insn_signum == SIGALRM || last_insn_signum == SIGPROF) {
                 range_bitmap_mark_timeout(&rb, insn);
