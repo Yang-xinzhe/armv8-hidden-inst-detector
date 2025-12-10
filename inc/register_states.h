87d08ae6bedb4d59fcb0ee73e6732b08b0d081d6
Yangxinzhe <1245314699@qq.com>
2025-12-10 18:02:05 +0800

    remove sp lr pc register observation

--- inc/register_states.h
+++ inc/register_states.h
@@ -4,7 +4,6 @@
 
 typedef struct __attribute__((aligned(4))) {
     uint32_t r0, r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12;
-    uint32_t sp, lr, pc;
     uint32_t cpsr;
 } RegisterStates;
 
