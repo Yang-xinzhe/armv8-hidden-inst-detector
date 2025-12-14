35104a05f12a3bf357e5279c27135c5f699c96f3
Yangxinzhe <1245314699@qq.com>
2025-12-14 21:45:33 +0800

    simd register

--- inc/register_states.h
+++ inc/register_states.h
@@ -7,4 +7,11 @@ typedef struct __attribute__((aligned(4))) {
     uint32_t cpsr;
 } RegisterStates;
 
+typedef struct __attribute__((aligned(16))) {
+    uint8_t q[16][16]; // 16 registers * 16 bytes
+    uint32_t fpscr;    // Floating-Point Status and Control Register
+    uint8_t _padding[12]; // Padding to align to 16 bytes (Total 272 bytes)
+} SimdRegisterStates;
+
 extern RegisterStates *reg_state_base_slot;
+extern SimdRegisterStates *simd_state_base_slot;
